bb956db642d07f2585e4281d1a833a60a86b572e
ThanVanDanhh <23130043@st.hcmuaf.edu.vn>
2025-11-24 12:16:23 +0700

    view game

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -4,25 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-import java.io.IOException;
 
 public class App extends Application {
-    @Override
-    // Trong App.java
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/colat.fxml"));
-        Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root);
 
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Tên file FXML của bạn (giả sử tên là "MainScene.fxml" và nằm trong classpath)
+        // Nếu file FXML nằm trong src/main/resources, bạn dùng "/"
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/colat.fxml"));
+
+        // Đặt Controller là class VideoBackgroundApp
+        loader.setControllerFactory(c -> {
+            if (c.equals(Game.class)) {
+                return new Game();
+            } else {
+                try {
+                    return c.getDeclaredConstructor().newInstance();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 880, 495);
+
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Video Background Demo");
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
-
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.colat {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
-
+    requires javafx.media;
 
     opens controller to javafx.fxml;
     exports controller;
